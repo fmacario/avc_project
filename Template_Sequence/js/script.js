4bbab362a5c2674fde94aee202eea38a9cb08aad
Bruno <bfhenriques178@gmail.com>
2017-03-12 22:30:11 +0000

    Template Sequence
    Configuration side

--- Template_Sequence/js/script.js
+++ Template_Sequence/js/script.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 	$("#generate").click(function () {	
 		var number_of_images = $("#number_of_images").val();
 		
+		$('#ask_number_div').hide();		
+		
+		$('#insert_img_div').append('<h4>Insira as imagens na ordem correcta:</h4>');
+		
 		for(var i=0; i<number_of_images; i++){
 			$("#insert_img_div").append('<div id="img'+i+'_div" class="col-sm-3 div-border single_img_div"> <input type="file" id="input'+i+'" onchange="readURL(this, '+i+')" /> </div>');
 		}
