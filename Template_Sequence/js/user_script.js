b0a14c6efce54f7ef21b1c8ddd7bdbe8219d950b
Bruno <bfhenriques178@gmail.com>
2017-03-14 00:17:37 +0000

    Template Sequence
    User side done; random display of preloaded images; only accepts correct order of the images

--- Template_Sequence/js/user_script.js
+++ Template_Sequence/js/user_script.js
@@ -1,30 +1,50 @@
-function start(num){
-    var i, col, height;
-    
+function start(num, images){
+    var i, col, height, temp, order;
+    var temp_images = images.slice();
+
     if(num>=5){
         height = 50;
-        /*if( (num%12) == 0 ){
-            col = 2;
-        }
-        else if( (num%4) == 0 || num%7 == 0){
-            col = 3;
-        }
-        else if( ((num%3) == 0) ){
-            col = 4;
-        }
-        else{*/
-            col = parseInt(12/num)+1;
-        //}
+        col = parseInt(12/num)+1;
     }
     else{
         col = 12/num;
         height = 100;
     }
-    console.log(col);
-    
+
     for(i=0; i<num; i++){
-        $('#choice_div').append('<div style="height:'+height+'%" id="choice'+i+'_div" class="col-sm-'+col+' single_img_div">choice</div>');
-        $('#answer_div').append('<div style="height:'+height+'%" id="answer'+i+'_div" class="col-sm-'+col+' single_img_div">answer</div>');
+        temp = Math.floor((Math.random() * temp_images.length));
+        order = images.indexOf(temp_images[temp]);
+        
+        //console.log(temp);
+        //console.log(order);
+        //console.log(temp_images[temp]);
+        //console.log(temp_images);
+        //console.log(images);
+
+        $('#choice_div').append('<div style="height:'+height+'%" id="choice'+i+'_div" class="col-sm-'+col+' single_img_div"> <img id="image'+order+'" src="'+temp_images[temp]+'" class="img" ondragstart="drag(event)"> </div>');
+        $('#answer_div').append('<div style="height:'+height+'%" id="answer'+i+'" class="col-sm-'+col+' single_img_div" ondrop="drop(event, this)" ondragover="allowDrop(event)"></div>');
+        
+        temp_images.splice(temp, 1);
     }
     
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, target) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    
+    imageId = data.split("").reverse()[0];
+    divId = target.id.split("").reverse()[0];
+
+    if(imageId == divId){
+        ev.target.appendChild(document.getElementById(data));
+    }
 }
