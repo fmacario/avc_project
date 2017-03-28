c063cb2ecab03d625c9a1467d3d967688f5eb656
Antonio Mota <antonio.mota@ua.pt>
2017-03-28 14:58:51 +0100

    clean rep

--- Plataform/src/app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
-];
-
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
