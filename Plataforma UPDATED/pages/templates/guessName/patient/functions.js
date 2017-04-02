3185dfcf2278cc41020e1ac425c2550054211adc
Antonio Mota <antonio.mota@ua.pt>
2017-04-02 16:45:13 +0100

    X

---
+++ Plataforma UPDATED/pages/templates/guessName/patient/functions.js
@@ -0,0 +1,185 @@
+var config = {
+    apiKey: "AIzaSyCWjs-R7KWD1Hqg1Ve4h1ZGynj06XbB-JQ",
+    authDomain: "avcproject-fae11.firebaseapp.com",
+    databaseURL: "https://avcproject-fae11.firebaseio.com",
+    storageBucket: "avcproject-fae11.appspot.com",
+    messagingSenderId: "1031859806052"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// References
+var storageRef = firebase.storage().ref(); // storage service
+var database = firebase.database(); // database service
+
+// read metadata
+var input;
+var nrLetters;
+var templatesRef = database.ref("templates/"); // database templates
+
+templatesRef.once("value", function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+        input = childSnapshot.val().inputa;
+        nrLetras = childSnapshot.val().nrLetras;
+        var arrayLetters = input.split('');
+        for (var i = 0; i < arrayLetters.length; i++) {
+            if (arrayLetters[i] == ' ') {
+                $("#inner").append("<div  class=\"row unselectable\" id=\"espaco" + i + "\">&nbsp;</div>");
+            }
+            else {
+                $("#inner").append("<div  class=\"child col-sm-1 unselectable\" id=\"letra" + i + "\">" + arrayLetters[i] + "</div>");
+            }
+        }
+        var letrasEscondidas = [];
+        var escondePosicao = [];        // posicao das letras a serem escondidas
+
+        /* nao deixa a posicao de esconder ser a mesma */
+        for (var i = 0; i < nrLetras; i++) {
+            var tmp = Math.floor(Math.random() * (input.length));
+
+            while (arrayLetters[tmp] == " ") {                  //previne que a letra a ser escondida seja um espaco
+                tmp = Math.floor(Math.random() * (input.length));
+            }
+
+            if (escondePosicao.length == 0) {                 //se array tiver limpo, poe
+                escondePosicao [i] = tmp;
+            } else {                                             //previne repeticao dentro do array
+                if ($.inArray(tmp, escondePosicao) != -1) {
+                    i--;
+                }
+                else {
+                    escondePosicao [i] = tmp;
+                }
+            }
+        }
+
+        /* poe background preto para esconder e mete as letras escondidas no array letrasescondidas */
+        /* atribui ás letras escondidas a chance de serem clicadas */
+        for (var i = 0; i < escondePosicao.length; i++) {
+            letrasEscondidas[i] = $("#letra" + escondePosicao[i]).html();
+            $("#letra" + escondePosicao[i]).attr('class', 'child col-sm-1 unselectable hided-div');
+            $("#letra" + escondePosicao[i]).attr('onclick', 'getSelectedDiv(id)');
+        }
+    });
+});
+
+
+// read image
+var spaceRef = storageRef.child('templates/guessname/teste.jpg');
+var path = spaceRef.fullPath;
+
+storageRef.child('templates/guessname/teste.jpg').getDownloadURL().then(function (url) {
+    var test = url;
+    document.querySelector('#imgid').src = test;
+}).catch(function (error) {
+
+});
+// acaba read image
+
+//quando botao e clicado
+$(":button").click(function () {
+    var clickedButton = this.id;
+    var selectedLetter = $("#" + selectedDiv).html();
+
+    checkIfDivSelected();
+
+    if (selectedLetter == clickedButton || selectedLetter == clickedButton.toUpperCase()) {
+        $("#" + selectedDiv).hide().fadeToggle(1000).attr('class', 'child col-sm-1 unselectable');
+        $("#" + selectedDiv).attr('onclick', '');
+        selectedDiv = null;
+        letrasEscondidas.remove(clickedButton);
+        letrasEscondidas.remove(clickedButton.toUpperCase());
+    }
+    else {
+        if (mensagensDoDoutor.length != 0) {  //usa mensagens dadas pela doutora se ela tiver dado
+            $("#message").html("<p id=\"textoAjuda\">" + mensagensDoDoutor[Math.floor(Math.random() * (mensagensDoDoutor.length))] + "</p>");
+        }
+        else {  //usa mensagens de ajuda predefinidas
+            $("#message").html("<p id=\"textoAjuda\">" + mensagensAjudaRandom[Math.floor(Math.random() * (mensagensAjudaRandom.length))] + "</p>");
+        }
+
+        $("#textoAjuda").delay(3000).fadeOut(1000);
+    }
+
+    checkIfDone();
+});
+
+/* function que esconde os caracteres */
+function esconderLetras() {
+
+    escondePosicao = [];        // posicao das letras a serem escondidas
+
+    /* nao deixa a posicao de esconder ser a mesma */
+    for (var i = 0; i < nrLetras; i++) {
+        var tmp = Math.floor(Math.random() * (input.length));
+
+        while (arrayLetters[tmp] == " ") {                  //previne que a letra a ser escondida seja um espaco
+            tmp = Math.floor(Math.random() * (input.length));
+        }
+
+        if (escondePosicao.length == 0) {                 //se array tiver limpo, poe
+            escondePosicao [i] = tmp;
+        } else {                                             //previne repeticao dentro do array
+            if ($.inArray(tmp, escondePosicao) != -1) {
+                i--;
+            }
+            else {
+                escondePosicao [i] = tmp;
+            }
+        }
+    }
+
+    /* poe background preto para esconder e mete as letras escondidas no array letrasescondidas */
+    /* atribui ás letras escondidas a chance de serem clicadas */
+    for (var i = 0; i < escondePosicao.length; i++) {
+        letrasEscondidas[i] = $("#letra" + escondePosicao[i]).html();
+        $("#letra" + escondePosicao[i]).attr('class', 'child col-sm-1 unselectable hided-div');
+        $("#letra" + escondePosicao[i]).attr('onclick', 'getSelectedDiv(id)');
+    }
+}
+
+
+/* Removes element from array by value*/
+Array.prototype.remove = function () {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+function checkIfDone() {
+    if (letrasEscondidas.length == 0) {
+        $("#message").html("<p id=\"textoAjuda\"><h3>MUITO BEM! CONCLUIO COM SUCESSO A TAREFA!</h3></p>");
+        $("button").prop('disabled', true);
+    }
+}
+
+function getSelectedDiv(div) {
+    if (selectedDiv == null) {
+        selectedDiv = div;
+        $("#" + selectedDiv).attr("class", "child col-sm-1 unselectable hided-selected-div");
+    }
+    else {
+        unselectLastDiv(selectedDiv);
+        selectedDiv = div;
+        $("#" + selectedDiv).attr("class", "child col-sm-1 unselectable hided-selected-div");
+    }
+
+}
+
+function unselectLastDiv(div) {
+    $("#" + div).attr("class", "child col-sm-1 unselectable hided-div");
+}
+
+function checkIfDivSelected() {
+    if (selectedDiv == null) {
+        $("#message").html("<p id=\"textoAjuda\">Seleccione um espaço preto para adivinhar a letra!</p>");
+        $("#textoAjuda").delay(2000).fadeOut(1000);
+        throw new Error("No black area selected");
+    }
+}
+
---
+++ Plataforma UPDATED/pages/templates/sequence/js/user_script.js
@@ -0,0 +1,120 @@
+var config = {
+    apiKey: "AIzaSyCWjs-R7KWD1Hqg1Ve4h1ZGynj06XbB-JQ",
+    authDomain: "avcproject-fae11.firebaseapp.com",
+    databaseURL: "https://avcproject-fae11.firebaseio.com",
+    storageBucket: "avcproject-fae11.appspot.com",
+    messagingSenderId: "1031859806052"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// References
+var storageRef = firebase.storage().ref(); // storage service
+var database = firebase.database(); // database service
+
+var templatesRef = database.ref("templates/sequence"); // database templates
+var order = []; // Array ordenado com nomes das imagens
+var images = [];
+
+templatesRef.once("value", function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+        order = childSnapshot.val().ordem;
+        for (var i = 0; i < order.length; i++) {
+            // read images
+            storageRef.child('templates/sequence/' + order[i]).getDownloadURL().then(function (url) {
+                images.push(url);
+            });
+        }
+    })
+});
+
+function start() {
+    $('#start').hide();
+    var orilength = images.length;
+
+    var col, height;
+
+    if (orilength >= 5) {
+        height = 50;
+        col = parseInt(12 / orilength) + 1;
+    }
+    else {
+        col = 12 / orilength;
+        height = 100;
+    }
+
+    for (var i = 0; i < orilength; i++) {
+        var temp = Math.floor((Math.random() * images.length));
+        console.log(images[temp]);
+        $('#choice_div').append('<div style="height:' + height + '%" id="choice' + i + '" class="col-sm-' + col + ' single_img_div"> <img id="img'+i+'" src="' + images[temp] + '" class="img" ondragstart="drag(event)"> </div>');
+        images.splice(temp, 1);
+        $('#answer_div').append('<div style="height:' + height + '%" id="'+ order[i] + '" class="col-sm-' + col + ' single_img_div" ondrop="drop(event, ' + orilength + ')" ondragover="allowDrop(event)"></div>');
+
+    }
+}
+
+
+/*
+ var i, col, height, temp, order;
+ var num = images.length;
+ var temp_images = images.slice();
+
+ if(num>=5){
+ height = 50;
+ col = parseInt(12/num)+1;
+ }
+ else{
+ col = 12/num;
+ height = 100;
+ }
+
+ for(i=0; i<num; i++){
+ temp = Math.floor((Math.random() * temp_images.length));
+ order = images.indexOf(temp_images[temp]);
+
+ $('#choice_div').append('<div style="height:'+height+'%" id="choice'+i+'" class="col-sm-'+col+' single_img_div"> <img id="image'+order+'" src="'+temp_images[temp]+'" class="img" ondragstart="drag(event)"> </div>');
+ $('#answer_div').append('<div style="height:'+height+'%" id="answer'+i+'" class="col-sm-'+col+' single_img_div" ondrop="drop(event, '+num+')" ondragover="allowDrop(event)"></div>');
+
+
+ }
+
+
+ }
+ */
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    console.log('drag');
+    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("path", ev.target.src);
+}
+
+var counter = 0;
+function drop(ev, num) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var path = ev.dataTransfer.getData("path");
+    var divId = ev.target.id;
+
+    var node = document.createTextNode("Tente Novamente!");
+
+    if ( $('#' + divId + ':has(img)').length == 0 ) {
+        if (path.includes(divId)) {
+
+            document.getElementById(divId).appendChild(document.getElementById(data));
+            counter++;
+            if (counter == num) {
+                alert('Parabéns! Tarefa concluída com sucesso!');
+            }
+        }
+        else {
+
+            document.getElementById(divId).appendChild(node);
+            setTimeout(function () {
+                document.getElementById(divId).innerHTML = "";
+            }, 1000);
+        }
+    }
+}
