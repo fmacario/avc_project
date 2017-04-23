18c8069cd533921b297111bca6e5088cbc2e97ee
Antonio Mota <antonio.mota@ua.pt>
2017-04-23 17:18:11 +0100

    up2

--- Plataforma UPDATED/pages/patients_side/choosetemp.js
+++ Plataforma UPDATED/pages/patients_side/choosetemp.js
@@ -2,30 +2,41 @@
 var database = firebase.database(); // database service
 var auth = firebase.auth();         // auth service
 
-firebase.auth().onAuthStateChanged(function(user) {
+var tpatients = $('#showtemplates')  // table patients
+var table = "<div id='showtemplates' class='box-body table-responsive no-padding'>" +
+  "<table class='table table-hover'>" +
+  "<tbody>" +
+  "<tr><th>Nome</th></tr>" +
+  "</tbody>" +
+  "</table>" +
+  "</div>";
+
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     var username = (user.email).split('@')[0];
     var refTemplates = database.ref('patients/' + username);
-        refTemplates.once("value", function (snapshot) {
-            for(var i=0; i<snapshot.val().ptemplates.length; i++){
-              var html_block =  "<div class='col-sm-3'>" +
-                                  "<button type='button' class='btn btn-primary btn-block btn-flat' style='height:150px;" + 
-                                    "onclick='redirect("+snapshot.val().ptemplates[i]+");'" +
-                                    "style='margin: auto;' id="+ snapshot.val().ptemplates[i]+">" + snapshot.val().ptemplates[i] + 
-                                  " </button>" + 
-                                "</div>";
-              $("#teste").append(html_block);  
-            }            
-        });
+
+    refTemplates.once("value", function (snapshot) {
+      for (var i = 0; i < snapshot.val().ptemplates.length; i++) {
+        tpatients.replaceWith(table);
+        var teste = $("#showtemplates table");
+        teste.append($('<tr id=' + snapshot.val().ptemplates[i] + ' onclick="redirect('+ snapshot.val().ptemplates[i] +')">')
+             .append($('<td>')
+             .text(snapshot.val().ptemplates[i])
+          )
+        )
+      }
+    });
   } else {
     // No user is signed in.
   }
 });
 
-function redirect(id){
+function redirect(id) {
+  console.log("Lol");
   var refTemplates2 = database.ref('templates/' + id.id);
   refTemplates2.once("value", function (snapshot) {
-             window.location = '../../pages/templates/'+snapshot.val().tipo+'/patient/patient.html' + '?param=' + id.id;
-        });
-   
+    window.location = '../../pages/templates/' + snapshot.val().tipo + '/patient/patient.html' + '?param=' + id.id;
+  });
+
 }
