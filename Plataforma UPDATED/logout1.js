2f3fa1fcf5f9e116183ceeec05e71afceac1c379
Bruno <bfhenriques178@gmail.com>
2017-04-23 16:01:32 +0100

    updates para o rotixz

--- Plataforma UPDATED/logout1.js
+++ Plataforma UPDATED/logout1.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCWjs-R7KWD1Hqg1Ve4h1ZGynj06XbB-JQ",
+    authDomain: "avcproject-fae11.firebaseapp.com",
+    databaseURL: "https://avcproject-fae11.firebaseio.com",
+    storageBucket: "avcproject-fae11.appspot.com",
+    messagingSenderId: "1031859806052"
+};
+
+firebase.initializeApp(config);
+
 // References
 var auth = firebase.auth();
 
