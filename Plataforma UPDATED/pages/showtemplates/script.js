9ea9fb56019b65e5dacc65650f7356d04ef62df2
Antonio Mota <antonio.mota@ua.pt>
2017-05-18 16:38:20 +0100

    seccao gestao de templates permite remover e fazer preview a templates

--- Plataforma UPDATED/pages/showtemplates/script.js
+++ Plataforma UPDATED/pages/showtemplates/script.js
@@ -1,22 +1,24 @@
 // References
 var database = firebase.database(); // database service
 
+$("#jogo").hide();
+
 // Displays templates of database in a table
 $(document).ready(function () {
-    $("#jogo").hide();
-
     var ttemplates = $('#showtemplates')  // table templates
     var templatesRef = database.ref("templates/"); // database templates
 
     templatesRef.once("value", function (snapshot) {
         snapshot.forEach(function (childSnapshot) {
             var teste = $("#jogo table");
-            teste.append($('<tr id="' + childSnapshot.key + '" onclick="preview()">')
+            teste.append($('<tr id="' + childSnapshot.key + '">')
                 .append($('<td>')
                     .text(childSnapshot.key)
                 )
                 .append($('<td>')
                     .text(childSnapshot.val().tipo)
+                )
+                .append($('<td><a><i class="fa fa-fw fa-remove" style="color:red; font-size: 20px" onclick="removeTemplate(this)"></i></a><a><i class="fa fa-fw fa-edit" style="font-size: 20px" onclick="editTemplate()"></i></a><a><i class="fa fa-fw fa-play-circle-o" style="color:green; font-size: 20px" onclick="previewTemplate(this)"></i></a>')
                 )
             )
         });
@@ -26,3 +28,21 @@ $(document).ready(function () {
         $("#jogo").show("slow");
     });
 });
+
+function removeTemplate(obj) {
+    var selectedTemplate = obj.parentElement.parentElement.parentElement.id;
+    templateRef = database.ref('templates/' + selectedTemplate);
+    templateRef.remove();
+
+    window.location.href = window.location.href;
+}
+
+function previewTemplate(obj) {
+    var selectedTemplate = obj.parentElement.parentElement.parentElement.id;
+    templateRef = database.ref('templates/' + selectedTemplate);
+    templateRef.once("value", function (snapshot) {
+        var pagina = snapshot.key;
+        var paginaespaco = pagina.replace(' ', '_');
+        window.location = '../templates/' + snapshot.val().tipo + '/patient/preview.html' + '?param=' + paginaespaco;
+    });
+}
